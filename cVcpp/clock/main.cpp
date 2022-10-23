10140ed1e91a72eb439ea78a275101b551048e66
mal1kc <59576495+mal1kc@users.noreply.github.com>
2022-10-23 17:27:24 +0300

    clock finished

--- cVcpp/clock/main.cpp
+++ cVcpp/clock/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include <unistd.h>
 int main() {
   using namespace std;
   const string digits[7][11] = {
@@ -20,41 +21,63 @@ int main() {
       {"┗━┛ ", "  ╹  ", " ┗━━ ", " ┗━┛ ", "   ╹ ", " ┗━┛ ", " ┗━┛ ", "   ╹ ",
        " ┗━┛ ", " ┗━┛ ", "   "},
   };
+  string output_buffer[7][8] = {
+      {"┏━┓ ", "┏━┓ ", "┏━┓ ", "┏━┓ ", "┏━┓ ", "┏━┓ ", "┏━┓ ", "┏━┓ "},
+      {"┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ "},
+      {"┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ "},
+      {"┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ "},
+      {"┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ "},
+      {"┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ ", "┃ ┃ "},
+      {"┗━┛ ", "┗━┛ ", "┗━┛ ", "┗━┛ ", "┗━┛ ", "┗━┛ ", "┗━┛ ", "┗━┛ "},
+  };
 
-  // for (int i = 0; i < 11; i++) {
-  //   for (int j = 0; j < 7; j++) {
-  //     cout << digits[j][i] << "\n";
-  //   }
-  // }
+  for (int i = 0; i < 11; i++) {
+    for (int j = 0; j < 7; j++) {
+      cout << digits[j][i] << "\n";
+    }
+  }
 
   time_t now = time(0);
   tm *ltm = localtime(&now);
-  time_t oglen = time(0);
-  tm *ogln = localtime(&oglen);
-  ogln->tm_hour = 13;
-  ogln->tm_min = 13;
 
   cout << put_time(ltm, "%a %b %d %H:%M:%S %Y") << "\n";
   cout << ltm->tm_hour << "\n";
 
-  tm *zaman = ogln;
+  tm *zaman = ltm;
+  while (true) {
 
-  if (zaman->tm_hour < 10) {
-    for (int i = 0; i < 7; i++)
-      cout << digits[i][zaman->tm_hour] << "\n";
-  } else if ((zaman->tm_hour >= 10) && (zaman->tm_hour < 20)) {
+    now = time(0);
+    zaman = localtime(&now);
     for (size_t i = 0; i < 7; i++) {
-      cout << digits[i][1] << digits[i][(zaman->tm_hour) % 10] << "\n";
+      output_buffer[i][0] = digits[i][(zaman->tm_hour) / 10];
+      output_buffer[i][1] = digits[i][(zaman->tm_hour) % 10];
     }
-  } else if ((zaman->tm_hour >= 20)) {
-    for (size_t i = 0; i < 7; i++) {
-      cout << digits[i][2] << digits[i][(zaman->tm_hour) % 10] << "\n";
-    }
-  }
-  for (size_t i = 0; i < 7; i++) {
-    cout << digits[i][10] << "\n";
-  }
 
+    for (size_t i = 0; i < 7; i++) {
+      output_buffer[i][2] = digits[i][10];
+    }
+
+    for (size_t i = 0; i < 7; i++) {
+      output_buffer[i][3] = digits[i][(zaman->tm_min) / 10];
+      output_buffer[i][4] = digits[i][(zaman->tm_min) % 10];
+    }
+
+    for (size_t i = 0; i < 7; i++) {
+      output_buffer[i][5] = digits[i][10];
+    }
+    for (size_t i = 0; i < 7; i++) {
+      output_buffer[i][6] = digits[i][(zaman->tm_sec) / 10];
+      output_buffer[i][7] = digits[i][(zaman->tm_sec) % 10];
+    }
+
+    for (size_t i = 0; i < 7; i++) {
+      for (size_t j = 0; j < 8; j++) {
+        cout << output_buffer[i][j];
+      }
+      cout << "\n";
+    }
+    sleep(1);
+  }
   // cout << digits[0][0] << "\n";
   // cout << digits[1][0] << "\n";
   // cout << digits[2][0] << "\n";
